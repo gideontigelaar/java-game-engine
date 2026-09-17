6c97b56b11e3db641ce65e7459b5c3f34f3886fa
Gideon Tigelaar <contact@gideontigelaar.com>
2026-09-17 14:38:23 +0200

    Add ShaderProgram for shader linking, and Mesh for drawing vertex data

---
+++ src/main/java/nl/team3/engine/graphics/ShaderProgram.java
@@ -0,0 +1,74 @@
+package nl.team3.engine.graphics;
+
+import org.joml.Matrix4f;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.FloatBuffer;
+
+import static org.lwjgl.opengl.GL20.*;
+
+public class ShaderProgram {
+    private final int programId;
+
+    public ShaderProgram(String vertexSource, String fragmentSource) {
+        int vertexShader = compile(GL_VERTEX_SHADER, vertexSource);
+        int fragmentShader = compile(GL_FRAGMENT_SHADER, fragmentSource);
+
+        programId = glCreateProgram();
+        glAttachShader(programId, vertexShader);
+        glAttachShader(programId, fragmentShader);
+        glLinkProgram(programId);
+
+        if (glGetProgrami(programId, GL_LINK_STATUS) == GL_FALSE) {
+            String log = glGetProgramInfoLog(programId);
+            throw new RuntimeException("Shader program failed to link:\n" + log);
+        }
+
+        glDetachShader(programId, vertexShader);
+        glDetachShader(programId, fragmentShader);
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+    }
+
+    private static int compile(int type, String source) {
+        int shader = glCreateShader(type);
+        glShaderSource(shader, source);
+        glCompileShader(shader);
+
+        if (glGetShaderi(shader, GL_COMPILE_STATUS) == GL_FALSE) {
+            String log = glGetShaderInfoLog(shader);
+            String kind = type == GL_VERTEX_SHADER ? "vertex" : "fragment";
+            throw new RuntimeException("Failed to compile " + kind + " shader:\n" + log);
+        }
+        return shader;
+    }
+
+    public void bind() {
+        glUseProgram(programId);
+    }
+
+    public void unbind() {
+        glUseProgram(0);
+    }
+
+    public int getUniformLocation(String name) {
+        return glGetUniformLocation(programId, name);
+    }
+
+    public void setUniformMat4(String name, Matrix4f matrix) {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            FloatBuffer buffer = stack.mallocFloat(16);
+            matrix.get(buffer);
+            glUniformMatrix4fv(getUniformLocation(name), false, buffer);
+        }
+    }
+
+    public void setUniform4f(String name, float x, float y, float z, float w) {
+        glUniform4f(getUniformLocation(name), x, y, z, w);
+    }
+
+    public void cleanup() {
+        unbind();
+        glDeleteProgram(programId);
+    }
+}
